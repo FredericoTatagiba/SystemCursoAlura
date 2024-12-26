905acf2183662cc2ae59541247be1814cc59d3ce
Frederico Ferreira Tatagiba <fredericotatagiba@gmail.com>
2024-12-26 15:11:36 -0300

    finalização

--- routes/web.php
+++ routes/web.php
@@ -19,23 +19,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return redirect('/series');
-})->middleware(Authenticator::class);
+Route::resource('/series', SeriesController::class)->except('show');
 
-Route::resource('/series', SeriesController::class)
-    ->except('show');
+Route::middleware('authenticator')->group(function(){
+    Route::get('/series/{series}', [SeriesController::class, 'show'])->name('series.show');
 
+    Route::get('/', function () {
+        return redirect('/series');
+    });
 
-Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])
-    ->name('seasons.index');
-
-Route::get('/seasons/{season}/episodes', [EpisodesController::class, 'index'])
-    ->name('episodes.index');
-
-Route::put('/seasons/{season}/episodes', [EpisodesController::class, 'watched'])
-    ->name('episodes.watched');
-
+    Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index');
+    Route::get('/seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
+    Route::put('/seasons/{season}/episodes', [EpisodesController::class, 'watched'])->name('episodes.watched');
+});
 
 Route::get('/login', [UserController::class,'index'])->name('login');
 Route::post('/login', [UserController::class,'login'])->name('signin');
@@ -55,4 +51,4 @@ Route::post('/register', [UserController::class,'store'])->name('user.store');
 //     Route::post('/series/salvar', 'store')->name('series.store');
 // });
 
-// Route::delete('/series/destroy/{id}', [SeriesController::class,'destroy'])->name('series.destroy');
+// Route::delete('/series/destroy/{id}', [SeriesController::class,'destroy'])->name('series.destroy'););
